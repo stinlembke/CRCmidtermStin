bc12d0232efaa566c13d9d2117e576e551e692cb
stin <klembke17@gmail.com>
2024-03-24 13:46:20 -0400

    trying very hard to add a transition to my 2nd sinwave

--- sketch.js
+++ sketch.js
@@ -18,14 +18,23 @@ function draw() {
   stroke(255);
   translate(width/2, height/2);
 
-  for(var c=300, i=0; i<200; i++, c--){
+  if(millis()>=5000){
+    strokeOpacity=map(millis(), 5000, 7000, 0, 100);
+  } else {
+    strokeOpacity = 100;
+  }
+
+  for(var i=0; i<175; i++){
     push()
     rotate(sin(frameCount+i/2)*100);
-    rect(0,0,500-i*3,500-1*3,100-1);
-    if(millis()>=5000){
-      stroke(100,100,c);
-      rotate(sin(frameCount+i/5)*150);
-    rect(0,0,500-i*3,500-1*3,100-1);
+    stroke(150,100,200);
+    rect(0, 0, 500 - i * 3, 500 - 1 * 3, 100 - 1);
+
+    if(millis()>=5000){ //2nd one appears, transitionally
+      // for(strokeOpacity=0;strokeOpacity<=100;strokeOpacity+=10){
+        stroke(80,200,170,strokeOpacity);
+        sin(frameCount + i / 5) * 100;
+        rect(0, 0, 400 - i * 2, 400 - 1 * 2, 80 - 1);
     }
     pop()
   }
